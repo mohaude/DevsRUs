91c9c6b1a1135446c8e711a2e5b593f1625cec2c
Moh <mohaudemars@outlook.com>
2024-12-13 21:32:30 +0100

    Delete models directory

--- models/Chat.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const ${model}Schema = new mongoose.Schema({
-  // Schema will be defined based on requirements
-}, { timestamps: true });
-
-module.exports = mongoose.model('${model}', ${model}Schema);
